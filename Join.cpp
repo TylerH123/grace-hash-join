b1e68a21e9982d6d13b8a975bd2006c870a40b5c
Jeff Lin <jeff.andrew.lin@gmail.com>
2022-12-06 14:13:18 -0500

    Progress on parititon phase

--- Join.cpp
+++ Join.cpp
@@ -1,5 +1,6 @@
 #include "Join.hpp"
 
+#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -10,9 +11,32 @@ using namespace std;
  */
 vector<Bucket> partition(Disk* disk, Mem* mem, pair<uint, uint> left_rel,
                          pair<uint, uint> right_rel) {
-	// TODO: implement partition phase
-	vector<Bucket> partitions(0, Bucket(disk)); // placeholder
-	return partitions;
+    // TODO: implement partition phase
+
+    vector<Bucket> partitions((MEM_SIZE_IN_PAGE - 1), Bucket(disk));  // placeholder
+    for (int i = left_rel.first; i < left_rel.second; ++i) {
+        mem->loadFromDisk(disk, i, 0);
+        Page* page = mem->mem_page(0);
+        for (int r = 0; r < page->size(); ++r) {
+            Record rec = page->get_record(r);
+            int hash = rec.partition_hash() % (MEM_SIZE_IN_PAGE - 1);
+            cout << hash << " ";
+            rec.print();
+        }
+    }
+
+    for (int i = right_rel.first; i < right_rel.second; ++i) {
+        mem->loadFromDisk(disk, i, 0);
+        Page* page = mem->mem_page(0);
+        for (int r = 0; r < page->size(); ++r) {
+            Record rec = page->get_record(r);
+            int hash = rec.partition_hash() % (MEM_SIZE_IN_PAGE - 1);
+            cout << hash << " ";
+            rec.print();
+        }
+    }
+
+    return partitions;
 }
 
 /*
@@ -20,7 +44,7 @@ vector<Bucket> partition(Disk* disk, Mem* mem, pair<uint, uint> left_rel,
  * Output: Vector of disk page ids for join result
  */
 vector<uint> probe(Disk* disk, Mem* mem, vector<Bucket>& partitions) {
-	// TODO: implement probe phase
-	vector<uint> disk_pages; // placeholder
-	return disk_pages;
+    // TODO: implement probe phase
+    vector<uint> disk_pages;  // placeholder
+    return disk_pages;
 }
